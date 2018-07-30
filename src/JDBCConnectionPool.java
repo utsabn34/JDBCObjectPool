925874a896313a24c8ca0d6f7fe2cfffd091f0b6
Utsab Neupane <utsabn34@gmail.com>
2018-07-30 17:12:18 +1000

    JDBCConnectionPool: method implemented of objectPool

--- src/JDBCConnectionPool.java
+++ src/JDBCConnectionPool.java
@@ -1,22 +1,62 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JDBCConnectionPool extends ObjectPool{
 
+    private String dsn, usr, pwd;
 
+    public JDBCConnectionPool( String driver, String dsn, String usr, String pwd )
+    {
+        try
+        {
+            Class.forName( driver ).newInstance();
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+        this.dsn = dsn;
+        this.usr = usr;
+        this.pwd = pwd;
+    }
 
 
     @Override
     Object create() {
-        return null;
+        try
+        {
+            return( DriverManager.getConnection( dsn, usr, pwd ) );
+        }
+        catch( SQLException e )
+        {
+            e.printStackTrace();
+            return( null );
+        }
     }
 
     @Override
     boolean validate(Object o) {
-        return  true;
+        try
+        {
+            return( ! ( ( Connection ) o ).isClosed() );
+        }
+        catch( SQLException e )
+        {
+            e.printStackTrace();
+            return( false );
+        }
     }
 
     @Override
     void expire(Object o) {
-
+        try
+        {
+            ( (Connection) o ).close();
+        }
+        catch( SQLException e )
+        {
+            e.printStackTrace();
+        }
     }
 }
